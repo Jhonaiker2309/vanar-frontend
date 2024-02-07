a186c68d3d5c861980940efda72fc96386830906
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-07 14:31:54 -0400

    Feat: Refactoring the progress panel

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import ProgressPanel from '../ProgressPanel/ProgressPanel';
+import { MessageSection } from '../MessageSection/MessageSection';
 
 const MainSection = () => {
   return (
-    <div className="w-2/3 h-full mt-[101px] overflow-scroll px-24 pt-36">
+    <div className="w-2/3 h-fit bg-black absolute top-[101px] px-36 pt-52 flex flex-col items-start gap-12">
       <ProgressPanel />
-    </div>);
+      <MessageSection />
+    </div>
+  );
 };
 
 export default MainSection;
--- src/components/ProgressPanel/ProgressPanel.tsx
+++ src/components/ProgressPanel/ProgressPanel.tsx
@@ -1,27 +1,66 @@
-import WeekCard from "../WeekCard/WeekCard";
+import { Icon } from '../Icon/Icon';
 
 const ProgressPanel = () => {
-    return (
-      <div>
-        <p className="text-white">19th Feb 2024 - 19th Apr 2024</p>
-        <div className="flex justify-between mb-4">
-            <h1 className="text-white text-6xl font-semibold">Velocity</h1>
-            <button className="bg-black text-white text-sm px-6 py-0 border rounded-full opacity-80 h-10">
-                Share
-            </button>            
-        </div>
-        <div className="bg-zinc-900	grid grid-cols-7 gap-x-2 px-1 py-2">
-         {
-          [1,2,3,4,5,6,7].map((value, i) => (
-            <div key={i}>
-              <WeekCard value={value} />
-              <div className="h-4 bg-blue-500 mt-4"></div>
-            </div>
-          ))
-         }
+  return (
+    <div className="w-full flex flex-col justify-start items-start">
+      <p className="text-lg text-[#a08cff] font-semibold">19th Feb 2024 - 19th Apr 2024</p>
+      <div className="w-full flex items-start justify-between">
+        <h1 className="text-white text-7xl font-semibold">Velocity</h1>
+        <button className="bg-[#0b0b0b] text-white font-bold text-sm px-4 py-0 border-2 border-[#4b4b4b] rounded-full h-10 flex items-center justify-center gap-4">
+          <Icon name="share" size={24} color="white" />
+          Share
+        </button>
+      </div>
+      <div className="w-full bg-[#0b0b0b] border-2 border-[#101010] rounded-xl mt-[74px] flex items-center justify-end gap-4 p-8">
+        <CreateCard status="Completed" week={1} />
+        <CreateCard status="In Progress" week={2} />
+        <CreateCard status="Locked" week={3} />
+        <CreateCard status="Locked" week={4} />
+        <CreateCard status="Locked" week={5} />
+        <CreateCard status="Locked" week={6} />
+        <CreateCard status="Locked" isFinal />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressPanel;
+
+interface CreateCardProps {
+  status: 'Completed' | 'Locked' | 'In Progress';
+  week?: number;
+  isFinal?: boolean;
+}
+
+const CreateCard = ({ status, week, isFinal = false }: CreateCardProps) => {
+  const isInProgress = status === 'In Progress';
+  const isCompleted = status === 'Completed';
+
+  return (
+    <div className="w-full flex flex-col justify-end items-center gap-8">
+      <div className="w-full p-4 rounded-xl flex flex-col justify-end items-center gap-4 bg-[#1a1a1a] ring-2 ring-[#4d4d4d] ">
+        <Icon
+          name={isFinal ? 'crown' : 'lock'}
+          size={48}
+          color={isCompleted ? '#a08cff' : isInProgress ? '#ecaa00' : ''}
+        />
+        <div className="flex flex-col justify-end items-center gap-2 text-white text-xs">
+          <p>{isFinal ? 'Congrats' : `Week 0${week}`}</p>
+
+          <div
+            className={`px-3 py-1 rounded-full  ${
+              isCompleted ? 'bg-[#a08cff]' : isInProgress ? 'bg-[#ecaa00]' : 'bg-[#4b4b4b]'
+            }`}
+          >
+            {status}
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default ProgressPanel;
+      <div
+        className={`w-full h-2 rounded-full bg- ${
+          isCompleted ? 'bg-[#a08cff]' : isInProgress ? 'bg-[#ecaa00]' : 'bg-[#4b4b4b]'
+        }`}
+      />
+    </div>
+  );
+};
