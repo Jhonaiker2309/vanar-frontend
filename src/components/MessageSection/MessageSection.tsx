a696d85a2e86d5f5b88af5aef159ded08ed8e671
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-07 14:34:40 -0400

    Feat: Creating message section - Pending of better name

---
+++ src/components/MessageSection/MessageSection.tsx
@@ -0,0 +1,23 @@
+export const MessageSection = () => {
+  return (
+    <div className="w-full flex flex-col items-start gap-8 text-white">
+      <h1 className="text-5xl font-semibold">
+        Welcome to foundation Link at Vanar Chain Testnet:Vanguard
+      </h1>
+      <p className="text-lg font-semibold py-1 px-3 bg-[#4d4d4d] rounded-full">
+        19th Feb 2024 - 19th Apr 2024
+      </p>
+      <img
+        className="w-full h-[360px] rounded-[40px]"
+        src="/images/message-background.png"
+        alt="week image"
+      />
+      <p className="text-lg text-[#4a4a4a]">
+        Hurry to join the greatest Valorant tournament of all time. Fight till the end and get the
+        reward. Hurry to join the greatest Valorant tournament of all time. Fight till the end and
+        get the reward. Hurry to join the greatest Valorant tournament of all time. Fight till the
+        end and get the reward.
+      </p>
+    </div>
+  );
+};
