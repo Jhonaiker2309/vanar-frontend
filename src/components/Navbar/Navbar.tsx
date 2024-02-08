f05a9c05f64eceec840f87a078dbe3f8103bc6cb
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-08 12:34:31 -0400

    Feat: Adding bg and navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,20 +4,17 @@ import { Web3Context } from '../../web3';
 const Navbar = () => {
   const { account, connectWeb3 } = useContext(Web3Context);
   return (
-    <header className="w-screen h-[101px] bg-[#0b0b0b] opacity-95 fixed top-0 left-0 border-b-2 border-b-[#4b4b4b] flex items-center justify-between px-8 z-50">
-      <div className="flex items-center gap-1">
-        <img src="images/vanar-logo.png" alt="Logo" />
-        <div className="flex items-start gap-4">
-          <button className="bg-[#a08cff] text-black font-bold py-1 px-2 rounded-lg ml-2">
-            VANGUARD
-          </button>
-          <div className="w-0.5 h-8 bg-[#4b4b4b]"></div>
-          <p className="text-white font-semibold">TESTNET</p>
-        </div>
+    <header className="w-screen h-[101px] bg-black opacity-95 fixed top-0 left-0 flex items-center justify-between px-14 z-50">
+      <div className="hidden md:flex items-center gap-1">
+        <img src="images/vanar-logo-full.svg" alt="Logo" />
+      </div>
+      <div className="flex md:hidden items-center gap-1">
+        {/* We need this logo to be provided  */}
+        <img src="images/vanar-logo.svg" alt="Logo" />
       </div>
       {!account && (
         <button
-          className="bg-white text-black font-semibold py-2 px-6 rounded-full opacity-100"
+          className="text-xs md:text-[18px] w-fit bg-white text-black font-semibold py-3 px-2 md:px-6 rounded-full opacity-100 "
           onClick={() => connectWeb3()}
         >
           Connect Wallet
