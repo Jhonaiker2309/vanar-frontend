5bc1b635da28236c7ab0d076e160594bbe0008ca
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-13 20:43:36 -0400

    Fix in nabvar opacity

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { Web3Context } from '../../web3';
 const Navbar = () => {
   const { account, connectWeb3 } = useContext(Web3Context);
   return (
-    <header className="w-screen h-[101px] bg-black opacity-90 fixed top-0 left-0 flex items-center justify-between px-8  md:px-14 z-50">
+    <header className="w-screen h-[101px] bg-[#0a0810] fixed top-0 left-0 flex items-center justify-between px-8  md:px-14 z-50">
       <div className="flex items-center gap-1">
         <img src="images/vanar-logo-full.svg" alt="Logo" className="w-4/5" />
       </div>
