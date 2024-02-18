caf442448ba8ecc391d9ad59f01a5b0fed6e3cdc
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-18 15:06:03 -0400

    Feat: Adding data for the nft

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from 'react';
 import MainSection from './components/MainSection/MainSection';
 import Navbar from './components/Navbar/Navbar';
 import SideImage from './components/SideImage/SideImage';
 
 const App = () => {
+  const [nftVideo, setNftVideo] = useState<{ video: string; name: string }>({
+    video: '',
+    name: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://vanar-backend.vercel.app');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setNftVideo({ video: data.currentVideo, name: data.currentNFT.name });
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="w-screen h-screen background overflow-scroll fixed">
       <Navbar />
@@ -11,7 +35,7 @@ const App = () => {
           <MainSection />
         </div>
         <div className="z-10">
-          <SideImage />
+          <SideImage nft={nftVideo} />
         </div>
       </div>
     </div>
