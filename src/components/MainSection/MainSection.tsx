146ac5b878e0ad8e2705d1bde80d6a268a697632
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-18 17:40:41 -0400

    Feat: Starting backend implementation for main section

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -2,7 +2,8 @@ import ProgressPanel from '../ProgressPanel/ProgressPanel';
 import { MessageSection } from '../MessageSection/MessageSection';
 import TasksList from '../TasksList/TasksList';
 
-const MainSection = () => {
+const MainSection = ({ data }: MainSectionProps) => {
+  const { tasks } = data;
   return (
     <div className="w-full md:w-2/3 h-fit bg-black opacity-90 md:absolute md:top-[101px] px-12 md:px-36 pt-52 pb-6 flex flex-col items-start gap-12">
       <ProgressPanel />
@@ -12,59 +13,19 @@ const MainSection = () => {
   );
 };
 
-// THIS IS DUMMY DATA
-const tasks = [
-  {
-    icon: 'fire',
-    text: 'Example 1',
-    reward: 10,
-    isCompleted: false,
-  },
-  {
-    icon: 'crown',
-    text: 'Example 2',
-    reward: 20,
-    isCompleted: true,
-    isBonus: true,
-  },
-  {
-    icon: 'hint',
-    text: 'Example 3',
-    reward: 10,
-    isCompleted: true,
-  },
-  {
-    icon: 'wallet',
-    text: 'Example 4',
-    reward: 10,
-    isCompleted: true,
-  },
-  {
-    icon: 'file',
-    text: 'Example 5',
-    reward: 10,
-    isCompleted: false,
-  },
-  {
-    icon: 'thunder',
-    text: 'Example 6',
-    reward: 20,
-    isCompleted: false,
-    isBonus: true,
-  },
-  {
-    icon: 'lock',
-    text: 'Example 7',
-    reward: 10,
-    isCompleted: false,
-  },
-  {
-    icon: 'interact',
-    text: 'Example 8',
-    reward: 20,
-    isCompleted: false,
-    isBonus: true,
-  },
-];
+interface MainSectionProps {
+  data: {
+    totalWeeks: number;
+    currentWeek: number;
+    isFinished: boolean;
+    tasks: {
+      isCompleted: boolean;
+      experience: number;
+      link?: string;
+      logo: string;
+      text: string;
+    }[];
+  };
+}
 
 export default MainSection;
