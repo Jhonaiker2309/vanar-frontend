e90c716684aa0de3cec001cd554892013958c58e
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-18 17:36:08 -0400

    Feat: Implementing backend connection for main section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,24 @@ import Navbar from './components/Navbar/Navbar';
 import SideImage from './components/SideImage/SideImage';
 
 const App = () => {
+  const [mainSectionData, setMainSectionData] = useState<{
+    totalWeeks: number;
+    currentWeek: number;
+    isFinished: boolean;
+    tasks: {
+      isCompleted: boolean;
+      experience: number;
+      link?: string;
+      logo: string;
+      text: string;
+    }[];
+  }>({
+    totalWeeks: 0,
+    currentWeek: 0,
+    isFinished: false,
+    tasks: [{ isCompleted: false, experience: 0, logo: '', text: '' }],
+  });
+
   const [nftVideo, setNftVideo] = useState<{ video: string; name: string }>({
     video: '',
     name: '',
@@ -17,7 +35,12 @@ const App = () => {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log(data);
+        setMainSectionData({
+          totalWeeks: data.numberOfWeeks,
+          currentWeek: data.currentWeekData.week,
+          isFinished: data.finished,
+          tasks: data.currentWeekData.tasks,
+        });
         setNftVideo({ video: data.currentVideo, name: data.currentNFT.name });
       } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
@@ -27,12 +50,14 @@ const App = () => {
     fetchData();
   }, []);
 
+  console.log(mainSectionData);
+
   return (
     <div className="w-screen h-screen background overflow-scroll fixed">
       <Navbar />
       <div className="w-full h-full flex flex-col md:flex-row">
         <div className="z-40">
-          <MainSection />
+          <MainSection data={mainSectionData} />
         </div>
         <div className="z-10">
           <SideImage nft={nftVideo} />
