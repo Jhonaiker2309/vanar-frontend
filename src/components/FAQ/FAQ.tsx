03bbc9e0c22ac210a8f7e508c2aa61dc21f24a86
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-19 00:52:37 -0400

    Fix: Change in FAQ titles to prevent console error

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -39,12 +39,12 @@ const QAndA = [
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. This privacy policy will inform you as to how we look after your personal data when you visit our website (regardless of where you visit it from and tell you about your privacy rights and how the law protects you, with no requirement that you have used the Service(s). This privacy policy is provided in a layered format so you can click through to the specific areas set out below.',
   },
   {
-    question: "What is Vanry technology DMCC's policy?",
+    question: "What is Vanry technology DMCC's policy 2?",
     answer:
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data.',
   },
   {
-    question: "What is Vanry technology DMCC's policy?",
+    question: "What is Vanry technology DMCC's policy 3?",
     answer:
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
   },
