94bd0cd44ae2b3723b225a29bb8523974c21b22c
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-19 23:15:14 -0400

    Implementing endpoint to track tasks

--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Bar } from '../Bar/Bar';
 import { Icon } from '../Icon/Icon';
 import Modal from '../Modal/Modal';
 import FAQ from '../FAQ/FAQ';
+import axios from 'axios';
+import { Web3Context } from '../../web3';
 
 const TasksList = ({ tasks }: TasksProps) => {
+  console.log(tasks);
   const [isOpenModal, setIsOpenModal] = useState(false);
-
   const handleOpenModal = () => {
     setIsOpenModal(!isOpenModal);
   };
@@ -35,12 +37,13 @@ const TasksList = ({ tasks }: TasksProps) => {
       <Bar percent={handleFillBar()} />
       <div className="w-full flex flex-col gap-4">
         {tasks.map((task, i) => {
-          const { logo, text, experience, isCompleted, isBonus } = task;
+          const { logo, text, experience, isCompleted, isBonus, link } = task;
           return (
             !isBonus && (
               <TaskCard
                 logo={logo}
                 text={text}
+                link={link}
                 experience={experience}
                 isCompleted={isCompleted}
                 key={i}
@@ -55,12 +58,13 @@ const TasksList = ({ tasks }: TasksProps) => {
           <Icon name="fire" size={40} />
         </div>
         {tasks.map((task, i) => {
-          const { logo, text, experience, isCompleted, isBonus } = task;
+          const { logo, text, experience, isCompleted, isBonus, link } = task;
           return (
             isBonus && (
               <TaskCard
                 logo={logo}
                 text={text}
+                link={link}
                 experience={experience}
                 isCompleted={isCompleted}
                 key={i}
@@ -91,9 +95,35 @@ interface TasksProps {
   tasks: Task[];
 }
 
-const TaskCard = ({ logo, text, experience, isCompleted = false }: Task) => {
+const TaskCard = ({ logo, text, experience, link, isCompleted = false }: Task) => {
+  const { account } = useContext(Web3Context);
+  const handleClickTask = async () => {
+    try {
+      const response = await axios.post('https://vanar-backend.vercel.app/visitLink', {
+        account: account,
+        link: link,
+      });
+
+      if (response.status === 200) {
+        window.open(link, '_blank');
+      } else {
+        throw new Error('Failed to visit link');
+      }
+    } catch (error) {
+      console.error('Error visiting link:', error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {}, [isCompleted]);
+
   return (
-    <div className="w-full ring-1 ring-[#4b4b4b] bg-[#1a1a1a] py-6 px-4 md:px-12 flex items-center justify-between rounded-[20px]">
+    <div
+      className={`w-full ring-1 ring-[#4b4b4b] bg-[#1a1a1a] py-6 px-4 md:px-12 flex items-center justify-between rounded-[20px] ${
+        link && 'cursor-pointer'
+      }`}
+      onClick={handleClickTask}
+    >
       <div className="flex items-center justify-start gap-4">
         <img src={logo} />
         <p className="text-sm md:text-lg">{text}</p>
