deef8eb5cbaf4d299fb86eaffc285f9e64a1f3db
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-25 14:46:09 -0400

    Implementing ranking on sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useContext, useRef, useEffect, useState } from 'react'; // Import useState
 import { Web3Context } from '../../web3';
+import Ranking from '../Ranking/Ranking';
 
 const SideImage = ({ nft }: SideImageProps) => {
   const { account, mintNFT } = useContext(Web3Context);
@@ -21,24 +22,27 @@ const SideImage = ({ nft }: SideImageProps) => {
 
   return (
     <div className="w-full md:w-1/3 h-full flex flex-col justify-center md:justify-start items-center bg-black opacity-90 md:fixed md:top-[101px] md:right-0 mb-24 md:mb-8 p-0  md:p-8 gap-1">
-      <video
-        ref={videoRef}
-        loop
-        muted={true}
-        className="w-4/5 rounded-3xl border-[1px] border-[#f6f6f6] relative z-0"
-      >
-        <source src={nftVideo} type="video/mp4" />
-      </video>
-      <div className="w-4/5 -mt-24 md:-mt-36 flex flex-col items-center z-20">
-        <p className="text-lg md:text-2xl text-white">{name}</p>
-        <p className="text-sm md:text-lg text-white">{experienceNeeded} VP</p>
-        <button
-          className="w-4/5 bg-[#A08CFF] text-black text-sm md:text-lg font-bold rounded-full py-1  md:py-4 text-center z-20"
-          onClick={() => mintNFT(account)}
+      <div className="w-full flex flex-col justify-center items-center md:mb-24">
+        <video
+          ref={videoRef}
+          loop
+          muted={true}
+          className="w-4/5 rounded-3xl border-[1px] border-[#f6f6f6] relative z-0"
         >
-          {account ? 'Claim NFT' : 'Connect Wallet'}
-        </button>
+          <source src={nftVideo} type="video/mp4" />
+        </video>
+        <div className="w-4/5 -mt-24 md:-mt-36 flex flex-col items-center z-20">
+          <p className="text-lg md:text-2xl text-white">{name}</p>
+          <p className="text-sm md:text-lg text-white">{experienceNeeded}XP</p>
+          <button
+            className="w-4/5 bg-[#A08CFF] text-black text-sm md:text-lg font-bold rounded-full py-1  md:py-4 text-center z-20"
+            onClick={() => mintNFT(account)}
+          >
+            {account ? 'Claim Reward' : 'Connect your account'}
+          </button>
+        </div>
       </div>
+      <Ranking top={dummyData} />
     </div>
   );
 };
@@ -48,3 +52,27 @@ interface SideImageProps {
 }
 
 export default SideImage;
+
+const dummyData = [
+  {
+    address: 'a1b2c3d4',
+    earnedVP: 100,
+  },
+
+  {
+    address: 'b2c3d4e5',
+    earnedVP: 90,
+  },
+  {
+    address: 'c3d4e5f6',
+    earnedVP: 50,
+  },
+  {
+    address: 'd4e5f6g7',
+    earnedVP: 20,
+  },
+  {
+    address: 'e5f6g7h8',
+    earnedVP: 0,
+  },
+];
