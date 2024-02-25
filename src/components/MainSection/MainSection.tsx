15e84711d76276f762192d50fc90358a0ddbd975
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-25 11:24:13 -0400

    Removing starting gap

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -19,7 +19,7 @@ const MainSection = ({
   };
 
   return (
-    <div className="w-full md:w-2/3 h-fit bg-black opacity-90 md:absolute md:top-[101px] px-12 md:px-36 pt-52 pb-6 flex flex-col items-start gap-12">
+    <div className="w-full md:w-2/3 h-fit bg-black opacity-90 md:absolute md:top-[101px] px-12 md:px-36 pt-8 pb-6 flex flex-col items-start gap-12">
       <ProgressPanel panelData={panelData} />
       <MessageSection />
       <TasksList tasks={tasks} />
