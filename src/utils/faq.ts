8a7a661d94dacc83f00e3145fedff67233ecb0d1
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-25 11:15:15 -0400

    Fix: New FAQ added

--- src/utils/faq.ts
+++ src/utils/faq.ts
@@ -1,16 +1,37 @@
 export const QAndA = [
   {
-    question: "What is Vanry technology DMCC's policy?",
+    question: 'What is velocity?',
     answer:
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. This privacy policy will inform you as to how we look after your personal data when you visit our website (regardless of where you visit it from and tell you about your privacy rights and how the law protects you, with no requirement that you have used the Service(s). This privacy policy is provided in a layered format so you can click through to the specific areas set out below.',
   },
   {
-    question: "What is Vanry technology DMCC's policy 2?",
+    question: 'What is VP?',
     answer:
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data.',
   },
   {
-    question: "What is Vanry technology DMCC's policy 3?",
+    question: 'How do I get the NFT for each week?',
+    answer:
+      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+  },
+  {
+    question: 'What are phases?',
+    answer:
+      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+  },
+  {
+    question: 'What are bonus tasks?',
+    answer:
+      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+  },
+  {
+    question: 'What is the schedule for the weeks?',
+    answer:
+      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+  },
+  {
+    question:
+      'Are there any airdrops or rewards for partaking in Velocity Vanguard testnet campaign?',
     answer:
       'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
   },
