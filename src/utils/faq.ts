68c6f687401b70d9d3cf020e86be0fd23b1576d4
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-25 16:44:34 -0400

    Adding faq answers

--- src/utils/faq.ts
+++ src/utils/faq.ts
@@ -2,37 +2,37 @@ export const QAndA = [
   {
     question: 'What is velocity?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. This privacy policy will inform you as to how we look after your personal data when you visit our website (regardless of where you visit it from and tell you about your privacy rights and how the law protects you, with no requirement that you have used the Service(s). This privacy policy is provided in a layered format so you can click through to the specific areas set out below.',
+      "The VELOCITY Campaign is an immersive testnet experience on the Vanar Chain, starting with foundational knowledge and expanding into areas like DeFi, NFTs, and gaming. It combines learning with action through weekly quests and challenges, offering rewards for participation and engagement. This initiative aims to deepen users' blockchain understanding while fostering innovation within the Vanar ecosystem.",
   },
   {
     question: 'What is VP?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data.',
+      'VP is velocity points, partake in tasks and bonus tasks to rank up in the leader boards and for the chance to win goodies across select campaigns.',
   },
   {
     question: 'How do I get the NFT for each week?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+      'By completing all the tasks for the week you become eligible for minting the NFT for the specific week. Once the week is over the NFT will no longer be mintable. ',
   },
   {
     question: 'What are phases?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+      'Phases are stages in part of VELOCITY Vanguard campaign. With each phase new tasks, new nft rewards geared towards learning and exploring Vanguard testnet.',
   },
   {
     question: 'What are bonus tasks?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+      'These are additional ways you can earn VP each week to help you get to the top of the leaderboards.',
   },
   {
     question: 'What is the schedule for the weeks?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+      'More details for upcoming weeks will be revealed as the weeks go live. Week 1 is the foundational week for Vanars Vanguard testnet launch. ',
   },
   {
     question:
       'Are there any airdrops or rewards for partaking in Velocity Vanguard testnet campaign?',
     answer:
-      'Vanry Technology DMCC provides blockchain services, and related Internet services (collectively, the “Service(s)”). The Service is operated by Vanry Technology DMCC (the “Company”, “we” or “us”) for users of the Service (“you/user”). The Company respects your privacy and is committed to protecting your personal data. ',
+      'In the VELOCITY Vanguard testnet campaign, rewards weave through the fabric of the unknown, with NFTs at each phase, offering a glimpse into deeper mysteries. These tokens, each a piece of a larger puzzle, hint at unseen potentials within the Vanar ecosystem. Beyond these enigmatic NFTs, the nature of further rewards remains cloaked in mystery, promising untold possibilities for those who venture into the campaign. With Vanar and friends, the only certainty is the allure of discovery, where the true value lies in the journey and the surprises it may unveil.',
   },
 ];
