8c1c511f41ef901d6ebdd27b8ed6de157edebb2f
Jhonatan Hernandez <jhonatanhernandez998@gmail.com>
2024-02-26 09:40:21 -0400

    b

--- src/web3/index.tsx
+++ src/web3/index.tsx
@@ -89,6 +89,8 @@ export const Web3Provider: React.FC<AppProviderProps> = ({ children }) => {
     if (!state.account || !state.contract) {
       return false;
     }
+    console.log('checking if minted');
+
     return await state.contract.getAlreadyMintedTimestampNFT(state.account, timestamp);
   }, []);
 
