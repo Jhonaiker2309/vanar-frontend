23a07b613f69a5686674090b08636e0693d907c7
Jhonatan Hernandez <jhonatanhernandez998@gmail.com>
2024-02-28 18:07:14 -0400

    restored rankings

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
-import { useContext, useRef, useEffect, useState } from 'react';
+import { useContext, useRef, useEffect, useState, useCallback } from 'react';
 import { Web3Context } from '../../web3';
-// import Ranking from '../Ranking/Ranking';
-// import axios from 'axios';
+import Ranking from '../Ranking/Ranking';
+import axios from 'axios';
 
 const SideImage = ({ nft, currentWeek }: SideImageProps) => {
   const { account, mintNFT, connectWeb3, checkIfAlreadyMinted } = useContext(Web3Context);
@@ -9,7 +9,8 @@ const SideImage = ({ nft, currentWeek }: SideImageProps) => {
   const { name, video } = nft;
   const [nftVideo, setNftVideo] = useState<string>(video);
   const [isClaimed, setIsClaimed] = useState<boolean>(false);
-  // const [rankedData, setRankedData] = useState<any[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [rankedData, setRankedData] = useState<any[]>([]);
 
   useEffect(() => {
     setNftVideo(video);
@@ -23,19 +24,19 @@ const SideImage = ({ nft, currentWeek }: SideImageProps) => {
     }
   }, [nftVideo]);
 
-  // const getRankedData = useCallback(async () => {
-  //   try {
-  //     const urlRankedData: string = import.meta.env.VITE_BACKEND_URL + '/rankedList';
-  //     const axiosData = await axios.get(urlRankedData);
-  //     setRankedData(axiosData.data.sortedList.slice(0, 5));
-  //   } catch (e) {
-  //     setRankedData([]);
-  //   }
-  // }, []);
+  const getRankedData = useCallback(async () => {
+    try {
+      const urlRankedData: string = import.meta.env.VITE_BACKEND_URL + '/rankedList';
+      const axiosData = await axios.get(urlRankedData);
+      setRankedData(axiosData.data.sortedList.slice(0, 5));
+    } catch (e) {
+      setRankedData([]);
+    }
+  }, []);
 
-  // useEffect(() => {
-  //   getRankedData();
-  // }, []);
+  useEffect(() => {
+    getRankedData();
+  }, []);
 
   useEffect(() => {
     const checkClaimed = async () => {
@@ -68,7 +69,7 @@ const SideImage = ({ nft, currentWeek }: SideImageProps) => {
           </button>
         </div>
       </div>
-      {/* <Ranking top={rankedData} /> */}
+      <Ranking top={rankedData} />
     </div>
   );
 };
