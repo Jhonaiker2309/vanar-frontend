1058fafc7dd7d807e734be63229b36c4fcdccdf3
Jhonaiker Blanco <blancojhonaiker@gmail.com>
2024-02-27 22:16:48 -0400

    Change to .env

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -5,13 +5,10 @@ export const fetchData = async (
   setMainSectionData: (value: MainSectionData) => void,
   setNftVideo: (value: NftData) => void,
 ) => {
-  /*const apiUrl = account
-    ? `https://vanar-backend.vercel.app/getData/${account}`
-    : 'https://vanar-backend.vercel.app/getData';*/
 
   const apiUrl = account
-    ? `https://vanar-backend.vercel.app/getData/${account}`
-    : 'https://vanar-backend.vercel.app/getData';    
+    ? `${process.env.REACT_APP_BACKEND_URL}/getData/${account}`
+    : `${process.env.REACT_APP_BACKEND_URL}/getData`;    
 
   try {
     const response = await axios.get(apiUrl);
--- src/utils/handleClickTask.ts
+++ src/utils/handleClickTask.ts
@@ -7,7 +7,7 @@ export const handleClickTask = async (
   link?: string
 ) => {
   try {
-    const response = await axios.post('https://vanar-backend.vercel.app/visitLink', {
+    const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/visitLink`, {
       account: account,
       link: link,
     });
