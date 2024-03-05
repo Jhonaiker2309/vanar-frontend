9895284281f27e36014bb31a56a194b0a2ef26e6
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-03-05 19:21:05 -0400

    New navbar design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
 import { Web3Context } from '../../web3';
+import { Icon } from '../Icon/Icon';
 
 const Navbar = ({ username }: { username?: string }) => {
   const { account, connectWeb3, disconnectWeb3 } = useContext(Web3Context);
@@ -34,10 +35,17 @@ const Navbar = ({ username }: { username?: string }) => {
           <h1>Leaderboard</h1>
         </NavLink>
       </div>
-      <div className=" flex items-center">
+      <div className="flex items-center gap-4">
+        <div className="h-12 flex justify-center items-center text-xs md:text-[18px] min-w-fit w-fit bg-[#A08CFF] bg-opacity-20  text-white ring-1 ring-[#A08CFF] font-semibold py-2 md:py-3 px-2 md:px-6 rounded-full gap-2">
+          <Icon name="thunder" size={24} color="white" />
+          <p className="text-nowrap">10VP</p>{' '}
+        </div>
+        <div className="h-12 flex justify-center items-center text-xs md:text-[18px] min-w-fit w-fit bg-[#A08CFF] bg-opacity-20  text-white ring-1 ring-[#A08CFF] font-semibold py-2 md:py-3 px-2 md:px-6 rounded-full gap-2">
+          <p className="text-nowrap">100 VG Staked</p>{' '}
+        </div>
         {username && !!account && <div className="text-white mx-10">@{username}</div>}
         <button
-          className=" text-xs md:text-[18px] min-w-fit w-fit bg-white text-black font-semibold py-2 md:py-3 px-2 md:px-6 rounded-full opacity-100 "
+          className="h-12 text-xs md:text-[18px] min-w-fit w-fit bg-white text-black font-semibold py-4 md:py-3 px-2 md:px-6 rounded-full opacity-100 "
           onClick={() => (!account ? connectWeb3() : disconnectWeb3())}
         >
           {!account ? 'Connect Wallet' : 'Disconnect'}
