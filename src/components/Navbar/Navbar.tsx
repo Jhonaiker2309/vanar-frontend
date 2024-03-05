2bc6931a0bbe67dd52d0e8ff43f19dec6a1e337d
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-03-05 17:52:34 -0400

    Feat: Change in navbar tags

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { useContext, useEffect } from 'react';
 import { Web3Context } from '../../web3';
 
@@ -9,11 +10,31 @@ const Navbar = ({ username }: { username?: string }) => {
     }
   }, []);
   return (
-    <header className="w-screen h-[101px] bg-[#0a0810] fixed top-0 left-0 flex items-center justify-between px-8  md:px-14 z-50">
+    <header className="w-screen h-[101px] bg-[#0a0810] fixed top-0 left-0 flex items-center justify-between px-8 md:px-14 gap-12 z-50">
       <div className="flex items-center gap-1">
         <img src="images/vanar-logo-full.svg" alt="Logo" className="w-4/5" />
       </div>
-      <div className="flex items-center">
+      <div className="flex justify-between items-center gap-16 text-2xl">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'text-[#A08CFF] font-bold' : 'text-[#39383a]')}
+        >
+          <h1>Campaign</h1>
+        </NavLink>
+        <NavLink
+          to="/claim"
+          className={({ isActive }) => (isActive ? 'text-[#A08CFF] font-bold' : 'text-[#39383a]')}
+        >
+          <h1>Claim Reward</h1>
+        </NavLink>
+        <NavLink
+          to="/leaderboard"
+          className={({ isActive }) => (isActive ? 'text-[#A08CFF] font-bold' : 'text-[#39383a]')}
+        >
+          <h1>Leaderboard</h1>
+        </NavLink>
+      </div>
+      <div className=" flex items-center">
         {username && !!account && <div className="text-white mx-10">@{username}</div>}
         <button
           className=" text-xs md:text-[18px] min-w-fit w-fit bg-white text-black font-semibold py-2 md:py-3 px-2 md:px-6 rounded-full opacity-100 "
