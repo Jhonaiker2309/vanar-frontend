807091160d7218857a5cbe8cb298cfff793340e7
Jhonatan Hernandez <jhonatanhernandez998@gmail.com>
2024-03-30 17:40:58 -0400

    fixed twitter issue

--- src/utils/handleClickTask.ts
+++ src/utils/handleClickTask.ts
@@ -6,30 +6,18 @@ export const handleClickTask = async (
   externalEndpoint: boolean,
   link?: string,
   isTwitterAPITask?: boolean,
-  completed?: boolean
+  completed?: boolean,
 ) => {
   try {
-
-    if(isTwitterAPITask && !account){
+    if (isTwitterAPITask && !account) {
       window.open(link, '_blank');
-    }
-    else if (isTwitterAPITask && account) {
-      const twitterUsername = localStorage.getItem('twitterUsername');
-
-      if(completed){
+    } else if (isTwitterAPITask && account) {
+      if (completed) {
         window.open(link, '_blank');
-      } 
-      else if (!twitterUsername) {
+      } else {
         const response = await axios.get(import.meta.env.VITE_TWITTER_ENDPOINT);
         const url = response.data.auth_url;
         window.location.href = url;
-      } else {
-        await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/addTweetsToAccount?wallet=${encodeURIComponent(
-            account,
-          )}&twitterUsername=${encodeURIComponent(twitterUsername)}`,
-        );
-        location.reload();
       }
       return;
     } else if (!externalEndpoint) {
